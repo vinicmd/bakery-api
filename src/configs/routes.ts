6036f7092f12aec85b3aea652ac3aa9d2154783b
Vinicius Duarte <viniciusduarte@outlook.com>
2024-03-21 11:03:11 +0000

    chore: create route

--- src/configs/routes.ts
+++ src/configs/routes.ts
@@ -1,11 +1,15 @@
 import { type Express, Router } from 'express'
 
+import { createUser } from '../controllers/user/create-user'
+
 export const router = Router()
 
 export default (app: Express): void => {
   const router = Router()
 
   router.get('/', (_req, res) => res.status(200).json({ message: 'alive' }))
+
+  router.post('/user', createUser)
 
   router.get('*', (_req, res) => res.sendStatus(404))
 
